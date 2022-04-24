ac9cab25ee73e498288ac65f2a43e504738db0da
Alec Johnson <alecjohnson55@gmail.com>
2022-04-24 09:16:32 -0700

    lint index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { useState, FormEvent } from "react";
-import moment from "moment";
-import Button from "@mui/material/Button";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { useState, FormEvent } from 'react'
+import moment from 'moment'
+import Button from '@mui/material/Button'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp'
 import {
   Avatar,
   FormControl,
@@ -14,48 +14,48 @@ import {
   ListItemText,
   Stack,
   TextField,
-} from "@mui/material";
-import React from "react";
-import { Post } from "../lib/common";
-import { v4 as uuidv4 } from "uuid";
+} from '@mui/material'
+import React from 'react'
+import { Post } from '../lib/common'
+import { v4 as uuidv4 } from 'uuid'
 
 const Home: NextPage = () => {
-  const [posts, setPosts] = useState<Post[]>([]); // list of posts in sorted order
-  const [newPostTitle, setNewPostTitle] = useState("");
+  const [posts, setPosts] = useState<Post[]>([]) // list of posts in sorted order
+  const [newPostTitle, setNewPostTitle] = useState('')
 
   // Create a new post, add it to posts array, sort by vote count.
   async function handleAddPost(event: FormEvent) {
-    event.preventDefault();
+    event.preventDefault()
     if (newPostTitle) {
-      setNewPostTitle("");
+      setNewPostTitle('')
       const post: Post = {
         id: uuidv4(), // random unique post id
         title: newPostTitle,
         date: Date.now(),
         votes: 1,
-      };
-      console.log("posting", post);
-      setPosts((posts) => [...posts, post].sort((a, b) => b.votes - a.votes));
+      }
+      console.log('posting', post)
+      setPosts((posts) => [...posts, post].sort((a, b) => b.votes - a.votes))
     }
   }
 
   // Upvote post with given id and re-sort posts array.
   async function handleUpvote(id: string) {
-    console.log("upvoting", id);
+    console.log('upvoting', id)
     setPosts((posts) =>
       posts
         .map((post: Post) =>
           post.id === id ? { ...post, votes: post.votes + 1 } : post
         )
         .sort((a, b) => b.votes - a.votes)
-    );
+    )
   }
 
   return (
     <Stack alignItems="center" minHeight="100vh" sx={{ mt: 10 }}>
       <form onSubmit={handleAddPost}>
         <FormControl>
-          <Stack direction={"row"}>
+          <Stack direction={'row'}>
             <TextField
               label="New post"
               sx={{ width: 520 }}
@@ -68,7 +68,7 @@ const Home: NextPage = () => {
           </Stack>
         </FormControl>
       </form>
-      <List sx={{ width: "100%", maxWidth: 600 }}>
+      <List sx={{ width: '100%', maxWidth: 600 }}>
         {posts.map((post: Post) => (
           <ListItem
             key={post.id}
@@ -89,7 +89,7 @@ const Home: NextPage = () => {
         ))}
       </List>
     </Stack>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
