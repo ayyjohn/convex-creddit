ccd0bcd2bb28d5b37b80892c88f751012928e1af
Alec Johnson <alecjohnson55@gmail.com>
2022-04-24 09:09:24 -0700

    add prettier

--- lib/common.ts
+++ lib/common.ts
@@ -1,8 +1,8 @@
-import { Id } from "convex-dev/values"
+import { Id } from 'convex-dev/values'
 
 export type Post = {
-  _id: Id; //convex-assigned ID
-  title: string;
-  date: number; // unix ms
-  votes: number;
-};
+  _id: Id //convex-assigned ID
+  title: string
+  date: number // unix ms
+  votes: number
+}
